d73101e91bc6daff68cfa97acca2563edae79c14
mac <mac@YHKo.local>
2020-12-23 21:31:46 +0800

    donut roll

---
+++ final/background.py
@@ -0,0 +1,9 @@
+import os, time, math, random, sys
+import pygame as pg
+from pygame.locals import *
+from pygame_functions import *
+
+class background:
+    screenSize(1250,650)
+    setAutoUpdate(False)
+    setBackgroundImage( [  ["mountains.png", "mountains.png"] ])
