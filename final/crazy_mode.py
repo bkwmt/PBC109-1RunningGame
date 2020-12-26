ac7991e0e82817093d2b7d743461cd9688bce599
mac <mac@YHKo.local>
2020-12-26 14:29:41 +0800

    new

---
+++ final/crazy_mode.py
@@ -0,0 +1,9 @@
+import os, time, math, random, sys
+import pygame as pg
+from pygame.locals import *
+from settings import *
+from players import *
+from platforms import *
+from enemies import *
+
+
