be1ef6a51a93dccb1a037743405923cb43dc6bc6
CHENG Ming <75352803+bkwmt@users.noreply.github.com>
2020-12-26 20:38:30 +0800

    Update main.py

--- final/main.py
+++ final/main.py
@@ -366,7 +366,7 @@ class Game:
                         sys.exit()      # 還有這裏
                     else:
                         go = False      # 停止迴圈
-                    #g.new()    # 寫這裡我都要按兩次才會結束誒
+                    g.new()    # 寫這裡我都要按兩次才會結束誒
             pg.display.update()
 
     def check_gameover(self):
