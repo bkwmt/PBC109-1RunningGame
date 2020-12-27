aa77f5ebd2e8f98170a30538b4690a689c5d8f7c
Emily-hen <jingrong1127@gmail.com>
2020-12-27 06:20:33 +0000

    Revert "Merge branch 'main' of https://github.com/bkwmt/PBC109-1RunningGame into main"
    This reverts commit 3830e60f304194ea1adff0d8749b994c395c1eea, reversing
    changes made to 29727b13bf3dc67912d1fdb3902148b4a6dcfcd3.

--- final/enemies.py
+++ final/enemies.py
@@ -1,6 +1,7 @@
 import random
 import pygame as pg
 from settings import *
+from players import *
 
 # 火球（正面飛行物）
 class Fireball(pg.sprite.Sprite):
@@ -21,6 +22,7 @@ class Fireball(pg.sprite.Sprite):
             self.rect.top = random.randint(50,600)
 
 # 上面掉落物
+"""
 class Dropdown(pg.sprite.Sprite):
 
     def __init__(self):
@@ -38,8 +40,53 @@ class Dropdown(pg.sprite.Sprite):
         else:
             self.rect.right = random.randint(50, (WIDTH - 50))
             self.rect.top = -500
+"""
+class Dropdown(pg.sprite.Sprite):
 
+    def __init__(self, filename, frames=1):
+        pg.sprite.Sprite.__init__(self)
+        self.images = []
+        img = loadImage(filename, img_W=280, img_H=70)
+        self.originalWidth = img.get_width() // frames
+        self.originalHeight = img.get_height()
+        frameSurf = pg.Surface((self.originalWidth, self.originalHeight), pg.SRCALPHA, 32)
+        x = 0
+        for frameNo in range(frames):
+            frameSurf = pg.Surface((self.originalWidth, self.originalHeight), pg.SRCALPHA, 32)
+            frameSurf.blit(img, (x, 0))
+            self.images.append(frameSurf.copy())
+            x -= self.originalWidth
+        self.image = pg.Surface.copy(self.images[0])
+        self.currentImage = 0
+        self.mask = pg.mask.from_surface(self.image)
+        self.angle = 0
+        self.scale = 1
+        self.rect = self.image.get_rect()
+        self.rect.right = random.randint(50, (WIDTH - 50))
+        self.rect.top = -500    # 從螢幕外掉進來
+
+    def update(self):
+        if self.rect.top < HEIGHT:
+            self.rect.top += DSPEED
+        else:
+            self.rect.right = random.randint(50, (WIDTH - 50))
+            self.rect.top = -500
+
+    def changeImage(self, index):
+        self.currentImage = index
+        if self.angle == 0 and self.scale == 1:
+            self.image = self.images[index]
+        else:
+            self.image = pg.transform.rotozoom(self.images[self.currentImage], -self.angle, self.scale)
+        oldcenter = self.rect.center
+        self.rect = self.image.get_rect()
+        originalRect = self.images[self.currentImage].get_rect()
+        self.originalWidth = originalRect.width
+        self.originalHeight = originalRect.height
+        self.rect.center = oldcenter
+        self.mask = pg.mask.from_surface(self.image)
 # 波狀飛行物
+"""
 class Strangebomb(pg.sprite.Sprite):
 
     def __init__(self):
@@ -62,3 +109,100 @@ class Strangebomb(pg.sprite.Sprite):
             self.rect.top = random.randint(150, HEIGHT - 150)
 
         self.rect.top -= self.speed_y
+"""
+class Strangebomb(pg.sprite.Sprite):
+
+    def __init__(self, filename, frames=1):
+        pg.sprite.Sprite.__init__(self)
+        self.images = []
+        img = loadImage(filename, img_W=560, img_H=70)
+        self.originalWidth = img.get_width() // frames
+        self.originalHeight = img.get_height()
+        frameSurf = pg.Surface((self.originalWidth, self.originalHeight), pg.SRCALPHA, 32)
+        x = 0
+        for frameNo in range(frames):
+            frameSurf = pg.Surface((self.originalWidth, self.originalHeight), pg.SRCALPHA, 32)
+            frameSurf.blit(img, (x, 0))
+            self.images.append(frameSurf.copy())
+            x -= self.originalWidth
+        self.image = pg.Surface.copy(self.images[0])
+        self.currentImage = 0
+        self.mask = pg.mask.from_surface(self.image)
+        self.angle = 0
+        self.scale = 1
+        self.rect = self.image.get_rect()
+        self.rect.right = WIDTH + 100
+        self.rect.top = random.randint(150, HEIGHT - 150)
+
+    def update(self):
+        # screen.blit(self.strangebomb , self.strangebomb_rect) 這行不需要
+        theta = pg.time.get_ticks()/170
+        self.speed_x = BSPEED
+        self.speed_y = WAVE * AMPLITUDE * math.sin(theta)
+        if self.rect.left >= -30:
+            self.rect.left -= self.speed_x
+        else:
+            self.rect.left = WIDTH + 100
+            self.rect.top = random.randint(150, HEIGHT - 150)
+
+        self.rect.top -= self.speed_y
+
+    def changeImage(self, index):
+        self.currentImage = index
+        if self.angle == 0 and self.scale == 1:
+            self.image = self.images[index]
+        else:
+            self.image = pg.transform.rotozoom(self.images[self.currentImage], -self.angle, self.scale)
+        oldcenter = self.rect.center
+        self.rect = self.image.get_rect()
+        originalRect = self.images[self.currentImage].get_rect()
+        self.originalWidth = originalRect.width
+        self.originalHeight = originalRect.height
+        self.rect.center = oldcenter
+        self.mask = pg.mask.from_surface(self.image)
+
+#地板怪物
+class GEnemy(pg.sprite.Sprite):
+
+    def __init__(self, filename, frames=1):
+        pg.sprite.Sprite.__init__(self)
+        self.images = []
+        img = loadImage(filename, img_W=560, img_H=70)
+        self.originalWidth = img.get_width() // frames
+        self.originalHeight = img.get_height()
+        frameSurf = pg.Surface((self.originalWidth, self.originalHeight), pg.SRCALPHA, 32)
+        x = 0
+        for frameNo in range(frames):
+            frameSurf = pg.Surface((self.originalWidth, self.originalHeight), pg.SRCALPHA, 32)
+            frameSurf.blit(img, (x, 0))
+            self.images.append(frameSurf.copy())
+            x -= self.originalWidth
+        self.image = pg.Surface.copy(self.images[0])
+        self.currentImage = 0
+        self.mask = pg.mask.from_surface(self.image)
+        self.angle = 0
+        self.scale = 1
+        self.rect = self.image.get_rect()
+        self.rect.right = WIDTH + 500
+        self.rect.top = 519
+
+    def update(self):
+        self.speed = PSPEED
+        if self.rect.right >= - 700:
+            self.rect.right -= self.speed
+        else:
+            self.rect.right = WIDTH + 80
+
+    def changeImage(self, index):
+        self.currentImage = index
+        if self.angle == 0 and self.scale == 1:
+            self.image = self.images[index]
+        else:
+            self.image = pg.transform.rotozoom(self.images[self.currentImage], -self.angle, self.scale)
+        oldcenter = self.rect.center
+        self.rect = self.image.get_rect()
+        originalRect = self.images[self.currentImage].get_rect()
+        self.originalWidth = originalRect.width
+        self.originalHeight = originalRect.height
+        self.rect.center = oldcenter
+        self.mask = pg.mask.from_surface(self.image)
